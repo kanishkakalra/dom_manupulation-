5e105585ba36304c6b75ccd0079dd08d71574edb
kanishka kalra <kanishkakalra5152@gmail>
2024-11-27 16:01:45 -0300

    made my own todo list

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-const input  = document.getElementById('todo-in');
+const inpu = document.getElementById('todo-in');
 const add = document.getElementById('add');
-
+//const del = document.getElementById('del');
 const list = document.getElementById('todo-list');
 
 
-
-add.addEventListener('click', (event)=>{
-    event.preventDefault(); 
-    const task = input.value.trim();
-   
-    if(task){
-        const li = document.createElement('li');
-        li.textContent = task;
-        list.appendChild(li);
-
-        const del = document.createElement('button');
-     del.innerText= 'Delete';
-     li.appendChild(del);
-
-     del.addEventListener('click',()=>{
-        li.remove();
-
-        
-    })
+add.addEventListener('click',(event)=>{
+ event.preventDefault();
+  const task = inpu.value.trim();
+  
+  if(task){
  
-     
-    
-       
-    }
-     input.value =" "
-   
-    
-});
+  const li = document.createElement('li');
+  li.textContent = task;
+  list.appendChild(li);
 
+   const del = document.createElement('button');
+   del.innerHTML='Delete';
+   li.appendChild(del)
+  del.addEventListener('click',()=>{
+    li.remove();
 
+  })
 
+  } 
+  inpu.value = '';
+})
